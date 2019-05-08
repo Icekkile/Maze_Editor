0dc4907af8688c8178f1b20287c623749c6284b7
icekkile <iceman.ryt@gmail.com>
2019-05-08 21:35:42 +0300

    biome serialisation

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -161,7 +161,6 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(419, 341);
             this.panel2.TabIndex = 3;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // trackBar2
             // 
